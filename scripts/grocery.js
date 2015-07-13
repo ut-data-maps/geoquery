76d9fa4522f84a543a30aaaa5bf57086a4cf4107
MetaSean <metasean@users.noreply.github.com>
2015-07-13 08:19:09 -0600

    Adds style-able marker
    It is still a static marker, but its color and internal icon can be programmatically modified.
    Uses:
     - npm install https://github.com/mapbox/maki#mb-pages --save
     - bower install Leaflet.MakiMarkers --save

--- scripts/grocery.js
+++ scripts/grocery.js
@@ -1,3 +1,11 @@
-L.marker([40.750001, -111.883334])
+/* Using:
+   https://www.mapbox.com/maki/ 
+   https://github.com/jseppi/Leaflet.MakiMarkers */
+/* Not using, but a possible alternative:
+   http://leafletjs.com/examples/custom-icons.htm */
+var groceryMarker = L.MakiMarkers
+				  .icon({icon: "grocery", color: "#0b0", size: "m"});
+
+L.marker([40.750001, -111.883334], {icon: groceryMarker})
 	.addTo(map)
 	.bindPopup("<b>Hello world!</b><br />I might be a grocery store.");
